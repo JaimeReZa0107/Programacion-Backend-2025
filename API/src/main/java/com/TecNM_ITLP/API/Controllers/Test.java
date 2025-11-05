0544cada432686a1ccc1cd4041ea3b9cf4950c43
Jaime Reyes <l22640176@piedad.tecnm.mx>
2025-11-05 13:38:30 -0600

    Commit para la classe Test.java la cual nos mostro el mensaje hola Api en el navegador

---
+++ API/src/main/java/com/TecNM_ITLP/API/Controllers/Test.java
@@ -0,0 +1,18 @@
+// ¡CAMBIA ESTA LÍNEA!
+package com.TecNM_ITLP.API.Controllers;
+
+// El resto es igual
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class Test {
+
+    @GetMapping("/hello")
+    public String helloworld(){
+        return "Hola API Rest";
+    }
+    
+}
