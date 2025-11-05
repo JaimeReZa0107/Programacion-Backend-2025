9220f3673f2295114b45c5735f2d47d781ee4ab7
Jaime Reyes <l22640176@piedad.tecnm.mx>
2025-11-05 17:01:01 -0600

    Commit para corregir errores

--- API/src/main/java/com/TecNM_ITLP/API/Controllers/Test.java
+++ API/src/main/java/com/TecNM_ITLP/API/Controllers/Test.java
@@ -1,7 +1,5 @@
-// ¡CAMBIA ESTA LÍNEA!
 package com.TecNM_ITLP.API.Controllers;
 
-// El resto es igual
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
